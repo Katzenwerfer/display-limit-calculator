8a2c277af6ba91b39f3bc859e07a717ab10daf5f
Katzenwerfer <53267697+Katzenwerfer@users.noreply.github.com>
2025-08-19 14:15:40 -0600

    Initial source commit Implements Special K's reflex formula

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include <array>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+double get_display_refresh_rate()
+{
+    std::cout << "Refresh rate: ";
+
+    double refresh_rate;
+    std::cin >> refresh_rate;
+
+    return refresh_rate;
+}
+
+double reflex_formula_limit(double refresh_rate)
+{
+    return refresh_rate - (refresh_rate * refresh_rate) / 3600.0;
+}
+
+void print_row(std::string label, double value, std::array<int, 4> width)
+{
+    std::cout << std::setw(width[0]) << (label + " |");
+    std::cout << std::setw(width[1] - 2) << static_cast<int>(value) << " |";
+    std::cout << std::setw(width[2] - 2) << static_cast<int>(std::round(value)) << " |";
+    std::cout << std::setw(width[3] - 2) << value << " |";
+    std::cout << '\n';
+}
+
+int main()
+{
+    double refresh_rate = get_display_refresh_rate();
+    if (!refresh_rate)
+    {
+        std::cerr << "Invalid refresh rate." << '\n';
+        return 1;
+    }
+
+    double reflex_fps = reflex_formula_limit(refresh_rate);
+
+    std::cout << std::fixed << std::setprecision(3) << std::right;
+
+    std::array<int, 4> width = {22, 16, 14, 21};
+
+    std::cout << "\n";
+    std::cout << std::setw(width[0]) << "Limit type |";
+    std::cout << std::setw(width[1]) << "Truncated FPS |";
+    std::cout << std::setw(width[2]) << "Rounded FPS |";
+    std::cout << std::setw(width[3]) << "Floating-point FPS |";
+    std::cout << '\n';
+
+    std::cout << std::string(width[0] - 1, '-') << "+";
+    std::cout << std::string(width[1] - 1, '-') << "+";
+    std::cout << std::string(width[2] - 1, '-') << "+";
+    std::cout << std::string(width[3] - 1, '-') << "+";
+    std::cout << '\n';
+
+    print_row("Reflex formula limit", reflex_fps, width);
+
+    return 0;
+}
